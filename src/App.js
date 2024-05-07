b42f68b23c6e1f37db905ce2426b6eadfd404d7b
Billy Arnold <billyarnoldd@gmail.com>
2024-05-07 22:00:16 +0100

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
+  const [ducks, setDucks] = useState([]);
+
+  useEffect(() => {
+    const fetchDucks = async () => {
+      try {
+        const res = await fetch("https://cataas.com/api/cats", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        //console.log(res);
+        const data = await res.json();
+        //console.log(data);
+        setDucks(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchDucks();
+  }, []);
+
+  if (!ducks) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex flex-wrap justify-center items-center ">
+      {ducks.map((duck) => (
+        <img
+          key={duck._id}
+          className="m-2 h-40 w-40 rounded-full object-cover"
+          src={`https://cataas.com/cat/${duck._id}`}
+        />
+      ))}
     </div>
   );
 }
